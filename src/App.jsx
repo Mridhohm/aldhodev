e91ad73df2629bbcdb6d2cb646e95c9c047d0d26
MRidho <helixvagabond0330@gmail.com>
2026-08-18 12:24:23 +0700

    Update email address in footer component

--- src/App.jsx
+++ src/App.jsx
@@ -188,7 +188,7 @@ export default function App() {
           </div>
           <div className="social-links">
             <a href="https://github.com/Mridhohm/aldhodev" target="_blank" rel="noopener noreferrer" className="social-link">GitHub Repo</a>
-            <a href="mailto:aldo.dev@gmail.com" className="social-link">aldo.dev@gmail.com</a>
+            <a href="mailto:aldo.dev@gmail.com" className="social-link">helixvagabond0330@gmail.com</a>
           </div>
         </div>
       </footer>
